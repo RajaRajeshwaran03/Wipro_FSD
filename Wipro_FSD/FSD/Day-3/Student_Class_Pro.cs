387155e4f5d48c3ec345552c7581a13e8fce5675
RajaRajeshwaran03 <rajeshwaranraja654@gmail.com>
2026-04-21 11:56:20 +0530

    Add project files.

---
+++ Wipro_FSD/FSD/Day-3/Student_Class_Pro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Student
+{
+
+    private string name;
+    private int id;
+
+    public Student(string name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Student Name: " + name);
+        Console.WriteLine("Student ID: " + id);
+    }
+
+    public void UpdateName(string newName)
+    {
+        name = newName;
+    }
+
+    public void UpdateId(int newId)
+    {
+        id = newId;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student1 = new Student("Rahul", 107);
+        Student student2 = new Student("Raja", 105);
+
+        Console.WriteLine("Original Student Details:");
+        student1.DisplayInfo();
+        student2.DisplayInfo();
+
+        student1.UpdateName("Rahul Rozario");
+        student1.UpdateId(207);
+
+        student2.UpdateName("Raja Rajeshwaran");
+        student2.UpdateId(205);
+
+        Console.WriteLine("Updated Student Details:");
+        student1.DisplayInfo();
+        student2.DisplayInfo();
+    }
+}
